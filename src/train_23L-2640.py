576f96979d6899baf869ddf5575a8fa9bbe6189e
Hafiz Abdullah <abdullahqasim607@gmail.com>
2026-09-06 23:04:07 +0500

    Add preprocessing normalization

--- src/train_23L-2640.py
+++ src/train_23L-2640.py
@@ -4,6 +4,7 @@ import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, r2_score
+from sklearn.preprocessing import StandardScaler
 
 
 STUDENT_ID = "23L-2640"
@@ -30,6 +31,11 @@ y = data["SalePrice"]
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 # Train model
 model = RandomForestRegressor(
